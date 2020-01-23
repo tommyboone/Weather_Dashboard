cc57c460ae8fad3739ce2d56faf919f7db5694b0
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-01-23 11:30:36 -0600

    created var for each type of weather data and appended to divs

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 
+
+$(document).ready(function(){
+
+    $("#submitWeather").on('click',function(event){
+        event.preventDefault();
+
+        var city = $('#city').val();
+        var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&APPID=49b107df79df951ca90870bc8b2042c1"
+             
+
+        if (city !== ''){
+            $.ajax({
+
+                url: queryURL,
+                method: "GET",
+                datatype: "jsonp",
+                success: function(data){
+                console.log(data);
+                console.log(data.main.temp); 
+
+                var cityName = (data.name);
+                $("#cityName").html(cityName);
+                
+
+                var icon = (data.weather[0].icon);
+                $("#icon").html("<img src='http://openweathermap.org/img/w/" + icon + ".png' alt='Icon depicting current weather.'>");
+
+                var temp = Math.round(data.main.temp);  
+                $("#temp-row").html("Temp:  " + temp + "&deg; F");
+              
+                var feelsLike =  Math.round(data.main.feels_like);
+                $("#feelsLike-row").html("Feels like:  " + feelsLike + "&deg; F");
+
+                var sky = (data.weather[0].main);
+                $("#sky-row").html("Condition:  " + sky);
+
+                var humidity = Math.round(data.main.humidity);
+                $("#humidity-row").html("Humidity: " + humidity + "%");
+
+                var windSpeed = Math.round(data.wind.speed);
+                $("#windSpeed-row").html("Wind Speed:  " + windSpeed + " MPH");
+  }
+            });
+        } else{
+            console.log("error");
+            alert("You must put in a city!");
+        }
+
+    });
+
+  
+});
